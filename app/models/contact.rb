03e47dcc3187eec88b12bd3bcc0086b866817d1a
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-05-16 12:29:46 -0300

    chore: as json override

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,15 @@
 class Contact < ApplicationRecord
-    belongs_to :kind
+  belongs_to :kind
+
+  def author
+    'Felipe Henssler Koetz'
+  end
+
+  def kind_description
+    kind.description
+  end
+
+  def as_json(_options = {})
+    super(methods: %i[author kind_description], root: true, include: { kind: { only: :description } })
+  end
 end
